a3fec2ead22342bc64aafb62432b13d8458d41f7
garretthamelin <garrett.hamelin@gmail.com>
2017-07-14 21:49:48 -0600

    fix gulpfiel and tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,50 @@
-const gulp     = require('gulp'),
-      bs       = require('browser-sync').create(),
-      plugins  = require('gulp-load-plugins')(),
-      runSeq   = require('run-sequence'),
-      del      = require('del'),
-      postCss  = require('gulp-postcss'),
-      shortCss = require('postcss-short'),
-      cssNext  = require('postcss-cssnext'),
-      cssSrcPath = 'src/assets/css/**/*.css',
-      cssDestPath = 'public/assets/css';
-      
+const gulp = require('gulp'),
+  bs = require('browser-sync').create(),
+  plugins = require('gulp-load-plugins')(),
+  runSeq = require('run-sequence'),
+  del = require('del'),
+  postCss = require('gulp-postcss'),
+  shortCss = require('postcss-short'),
+  cssNext = require('postcss-cssnext'),
+  cssSrcPath = 'src/assets/css/**/*.css',
+  cssDestPath = 'public/assets/css';
+
 
 
 
 // CSS Task
 
-gulp.task('compile-css', function() {
+gulp.task('compile-css', function () {
   const plugs = [shortCss, cssNext];
 
   return gulp.src([cssSrcPath])
 
-  .pipe(plugins.sourcemaps.init())
-  .pipe(postCss(plugs))
-  .pipe(plugins.concat('lib.min.css'))
-  .pipe(plugins.cleanCss())
-  .pipe(plugins.sourcemaps.write('.'))
-  .pipe(gulp.dest(cssDestPath))
-  .pipe(bs.stream());
+    .pipe(plugins.sourcemaps.init())
+    .pipe(postCss(plugs))
+    .pipe(plugins.concat('lib.min.css'))
+    .pipe(plugins.cleanCss())
+    .pipe(plugins.sourcemaps.write('.'))
+    .pipe(gulp.dest(cssDestPath))
+    .pipe(bs.stream());
 });
 
 // gulp browser-sync task
-gulp.task('serve', function() {
+gulp.task('serve', function () {
   bs.init({
     server: {
-      baseDir:"./"
+      baseDir: "./"
     }
   });
-});
 
-
-gulp.task('html', function() {
-  
-  gulp.watch('index.html').on('change',bs.reload());
-});
-
+})
 // gulp watch
-gulp.task('watch', function() {
+gulp.task('watch', function () {
   gulp.watch([cssSrcPath], ['compile-css']);
-  gulp.watch(['./index.html'], ['html']);
+  gulp.watch('index.html').on('change', bs.reload);
 });
 
 
 //gulp default
-gulp.task('default', function() {
-  runSeq('serve','compile-css', 'watch')
+gulp.task('default', function () {
+  runSeq('serve', 'compile-css', 'watch')
 });
