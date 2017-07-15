ca9e3f1b413659d7cb36b2f93ba8438e9d2c1157
garretthamelin <garrett.hamelin@gmail.com>
2017-07-14 21:13:41 -0600

    add dependencies for gulp add gulpfile and add tasks

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+const gulp     = require('gulp'),
+      bs       = require('browser-sync'),
+      plugins  = require('gulp-load-plugins')(),
+      runSeq   = require('run-sequence'),
+      del      = require('del'),
+      postCss  = require('gulp-postcss'),
+      shortCss = require('postcss-short'),
+      cssNext  = require('postcss-cssnext'),
+      cssSrcPath = 'src/assets/css/**/*.css',
+      cssDestPath = 'public/assets/css';
+
+
+
+// CSS Task
+
+gulp.task('compile-css', function(){
+  const plugs = [shortCss, cssNext];
+
+  return gulp.src([cssSrcPath])
+
+  .pipe(plugins.sourcemaps.init())
+  .pipe(postCss(plugs))
+  .pipe(plugins.concat('lib.min.css'))
+  .pipe(plugins.cleanCss())
+  .pipe(plugins.sourcemaps.write('.'))
+  .pipe(gulp.dest(cssDestPath))
+});
+
+
+// gulp watch
+gulp.task('watch', function(){
+  gulp.watch([cssSrcPath], ['compile-css']);
+})
+
+
+//gulp default
+gulp.task('default', ['compile-css', 'watch'])
